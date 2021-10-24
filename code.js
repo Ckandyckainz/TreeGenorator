c95981900a546d096b558a2c7797551bd456f2ee
Ckandyckainz <74159422+Ckandyckainz@users.noreply.github.com>
2021-10-23 19:40:21 -0700

    branchBend
    added branchBend and branchBendVary

--- code.js
+++ code.js
@@ -9,10 +9,12 @@ class TreeData{
         this.branchColor = colorString(Math.random(), Math.random(), Math.random(), 1);
         this.leafColor = colorString(Math.random(), Math.random(), Math.random(), 1);
         this.tiers = randomBetween(3, 8, 1);
-        this.growth = randomBetween(1, 4, 1);
+        this.growth = randomBetween(2, 4, 1);
         this.angleVary = randomBetween(Math.PI/8, Math.PI/2, 0.01);
-        this.branchThickness = randomBetween(3, 12, 1);
-        this.branchThicknessProportions = randomBetween(1, 2, 0.01);
+        this.branchThickness = randomBetween(6, 19, 1);
+        this.branchThicknessProportions = randomBetween(1.5, 2, 0.01);
+        this.branchBend = Math.random()*3-1.5;
+        this.branchBendVary = Math.random();
     }
 }
 
@@ -37,19 +39,30 @@ function generateButtonClicked(){
 generateButton.addEventListener("click", generateButtonClicked);
 
 function drawTree(treeData, ctx, w, h){
-    drawBranch(treeData, 0, Math.PI/-2, 0.5, 0.7, ctx, w, h);
+    drawBranch(treeData, 0, Math.PI/-2, Math.PI/-2, 0.5, 0.7, ctx, w, h);
 }
 
-function drawBranch(treeData, tier, angle, x, y, ctx, w, h){
+function drawBranch(treeData, tier, angle, targetAngle, x, y, ctx, w, h){
     ctx.strokeStyle = treeData.branchColor;
     ctx.lineWidth = treeData.branchThickness/(treeData.branchThicknessProportions**tier);
     ctx.beginPath();
     ctx.moveTo(x*w, y*h);
-    ctx.lineTo((x+Math.cos(angle)/treeData.tiers/2)*w, (y+Math.sin(angle)/treeData.tiers/2)*h);
+    let xCounter = x;
+    let yCounter = y;
+    let currentAngle = angle;
+    for (let i=0; i<10; i++) {
+        currentAngle += (targetAngle-angle)/10;
+        xCounter += Math.cos(currentAngle)/treeData.tiers/20;
+        yCounter += Math.sin(currentAngle)/treeData.tiers/20;
+        ctx.lineTo(xCounter*w, yCounter*h);
+    }
     ctx.stroke();
     if (treeData.tiers > tier) {
         for (let i=0; i<randomBetween(treeData.growth-1, treeData.growth+1, 1); i++) {
-            drawBranch(treeData, tier+1, randomBetween(angle-treeData.angleVary, angle+treeData.angleVary, 0.01), x+Math.cos(angle)/treeData.tiers/2, y+Math.sin(angle)/treeData.tiers/2, ctx, w, h);
+            let newTargetAngle = randomBetween(currentAngle-treeData.angleVary, currentAngle+treeData.angleVary, 0.01);
+            let branchBend = treeData.branchBend+randomBetween(-treeData.branchBendVary, treeData.branchBendVary, 0.01);
+            let startingAngle = currentAngle+(newTargetAngle-currentAngle)*branchBend;
+            drawBranch(treeData, tier+1, startingAngle, newTargetAngle, xCounter, yCounter, ctx, w, h);
         }
     }
 }
