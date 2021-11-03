dcf6ef1c33bc8347a3d35146989d98195072628c
Ckandyckainz <74159422+Ckandyckainz@users.noreply.github.com>
2021-11-03 16:34:31 -0700

    branchWave
    Added branch waves, which are a larger more uniform version of wobbles. A sin-wave is added to the x-coordinates to make it wavy. Added branchWaveSize, branchWaveLength, branchWaveSizeVary (represented as "bwsm" modifier), and branchWaveLengthVary (represented as "bwlm" modifier).

--- code.js
+++ code.js
@@ -12,7 +12,7 @@ class TreeData{
         this.angleVary = randomBetween(Math.PI/8, Math.PI/2, 0.01);
         this.branchThickness = randomBetween(6, 19, 1);
         this.branchThicknessProportions = randomBetween(1.5, 2, 0.01);
-        this.branchBend = Math.random()*3-1.5;
+        this.branchBend = (Math.random()*3-1.5)+1;
         this.branchBendVary = Math.random();
         this.branchWobble = Math.random()*8;
         this.tierSmoothness = Math.random();
@@ -23,9 +23,10 @@ class TreeData{
         this.branchSegmentLengthVary = Math.random()*0.7;
         this.branchLengthVary = Math.random()*0.7;
         this.branchContinue = Math.random();
-        this.branchWaveSize = 0;
-        this.branchWaveLength = 0;
-        this.branchWaveVary = 0;
+        this.branchWaveSize = Math.random()*4;
+        this.branchWaveLength = Math.random()*1.5;
+        this.branchWaveSizeVary = Math.random()/2;
+        this.branchWaveLengthVary = Math.random()/2;
     }
 }
 
@@ -91,10 +92,10 @@ function generateButtonClicked(){
 generateButton.addEventListener("click", generateButtonClicked);
 
 function drawTree(treeData, ctx, m, x, y, tX, tY, trunkY, light){
-    drawBranch(treeData, 0, Math.PI/-2, Math.PI/-2, x, y, tX, tY, ctx, m, trunkY, light);
+    drawBranch(treeData, 0, Math.PI/-2, Math.PI/-2, x, y, tX, tY, ctx, m, trunkY, light, 0);
 }
 
-function drawBranch(treeData, tier, angle, targetAngle, x, y, tX, tY, ctx, m, trunkY, light){
+function drawBranch(treeData, tier, angle, targetAngle, x, y, tX, tY, ctx, m, trunkY, light, waveCounter){
     let bm = 1+randomBetween(-treeData.branchLengthVary, treeData.branchLengthVary, 0.01);
     let s = treeData.branchSegments+randomBetween(-1*treeData.branchSegmentsVary, treeData.branchSegmentsVary, 1);
     let z = (trunkY*5-1)/4;
@@ -104,7 +105,16 @@ function drawBranch(treeData, tier, angle, targetAngle, x, y, tX, tY, ctx, m, tr
     let yCounter = y;
     let currentAngle = angle;
     let lastWobble = 0;
+    let wave = waveCounter;
+    let bwsm = 0;
+    let bwlm = 0;
+    let oldBwsm = 0;
+    let oldBwlm = 0;
     for (let i=0; i<s; i++) {
+        oldBwsm = bwsm;
+        oldBwlm = bwlm;
+        bwsm = 1+Math.random()*treeData.branchWaveSizeVary;
+        bwlm = 1+Math.random()*treeData.branchWaveLengthVary;
         let slm = 1+randomBetween(-treeData.branchSegmentLengthVary, treeData.branchSegmentLengthVary, 0.01);
         let r0 = (treeData.branchColor[0]*z*a+light[0]*b)*(1-Math.random()*treeData.branchColorVary);
         let g0 = treeData.branchColor[1]*z*a+light[1]*b*(1-Math.random()*treeData.branchColorVary);
@@ -112,15 +122,16 @@ function drawBranch(treeData, tier, angle, targetAngle, x, y, tX, tY, ctx, m, tr
         ctx.strokeStyle = colorString(r0, g0, b0, 1);
         ctx.lineWidth = treeData.branchThickness/(treeData.branchThicknessProportions**(tier+i*treeData.tierSmoothness/s));
         ctx.beginPath();
-        ctx.moveTo(xCounter*m+tX, yCounter*m+tY);
+        ctx.moveTo(xCounter*m+tX+Math.sin(wave*treeData.branchWaveLength*oldBwlm)*treeData.branchWaveSize*oldBwsm, yCounter*m+tY);
         let wobble = randomBetween(-1*treeData.branchWobble, treeData.branchWobble, 0.01);
+        wave ++;
         currentAngle += ((targetAngle-angle)+wobble-lastWobble)/s;
         lastWobble = wobble;
         xCounter += Math.cos(currentAngle)*slm*bm/treeData.tiers/s/2;
         yCounter += Math.sin(currentAngle)*slm*bm/treeData.tiers/s/2;
         let xExtend = xCounter+Math.cos(currentAngle)*slm*bm/treeData.tiers/treeData.segmentExtend;
         let yExtend = yCounter+Math.sin(currentAngle)*slm*bm/treeData.tiers/treeData.segmentExtend;
-        ctx.lineTo(xExtend*m+tX, yExtend*m+tY);
+        ctx.lineTo(xExtend*m+tX+Math.sin(wave*treeData.branchWaveLength*bwlm)*treeData.branchWaveSize*bwsm, yExtend*m+tY);
         ctx.stroke();
     }
     if (treeData.tiers > tier) {
@@ -137,7 +148,7 @@ function drawBranch(treeData, tier, angle, targetAngle, x, y, tX, tY, ctx, m, tr
             }
             let branchBend = treeData.branchBend+randomBetween(-1*treeData.branchBendVary, treeData.branchBendVary, 0.01);
             let startingAngle = currentAngle+(newTargetAngle-currentAngle)*branchBend;
-            drawBranch(treeData, tier+1, startingAngle, newTargetAngle, xCounter, yCounter, tX, tY, ctx, m, trunkY, light);
+            drawBranch(treeData, tier+1, startingAngle, newTargetAngle, xCounter, yCounter, tX, tY, ctx, m, trunkY, light, wave);
         }
     }
 }
